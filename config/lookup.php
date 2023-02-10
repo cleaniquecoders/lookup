8da049413ec4088c242a60f80b052e65253b3019
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-02-11 01:16:12 +0800

    Update

--- config/lookup.php
+++ config/lookup.php
@@ -5,5 +5,5 @@ use CleaniqueCoders\Lookup\Models\Lookup;
 return [
     'model' => Lookup::class,
     'cache_duration' => env('LOOKUP_CACHE_DURATION', 60),
-    'seeder' => base_path('app/lookup.json'),
+    'seeder' => storage_path('app/lookup.json'),
 ];
